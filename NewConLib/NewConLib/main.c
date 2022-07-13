2639794e2100205662a8bcb40c2a5424fd5a4b67
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-07-13 11:59:04 +0300

    impl ConLibSetTextColor

--- NewConLib/NewConLib/main.c
+++ NewConLib/NewConLib/main.c
@@ -6,6 +6,7 @@ int main()
     ConLibSetTitle(_T("NewConLib Example"));
     ConLibSetWinSize(30, 60);
     ConLibSetCursorLocation(2, 2);
+    ConLibSetTextColor(FOREGROUND_RED, BACKGROUND_GREEN);
     ConLibWriteString(_T("你好世界\n"));
     ConLibWriteChar(_T('H'));
 #ifdef _DEBUG
