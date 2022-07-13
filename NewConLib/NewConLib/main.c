aac75a1f54cf7ab20b856ba6ac1244c12ff13706
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-07-13 11:50:13 +0300

    impl ConLibSetTitle, ConLibSetWinSize

--- NewConLib/NewConLib/main.c
+++ NewConLib/NewConLib/main.c
@@ -3,10 +3,10 @@
 
 int main()
 {
+    ConLibSetWinSize(30, 60);
+    ConLibSetTitle(_T("NewConLib Example"));
     ConLibWriteString(_T("你好世界\n"));
     ConLibWriteChar(_T('H'));
-
-
     system("pause");
     return 0;
 }
