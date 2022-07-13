b1e2d02199a169c89affc5245aa22703908942be
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-07-13 12:11:41 +0300

    impl ConLibGetCursorLocation

--- NewConLib/NewConLib/main.c
+++ NewConLib/NewConLib/main.c
@@ -7,7 +7,9 @@ int main()
 {
     ConLibSetTitle(_T("NewConLib Example"));
     ConLibSetWinSize(30, 60);
-    ConLibSetCursorLocation(2, 2);
+    ConLibSetCursorLocation(4, 2);
+    int cursorPosX, cursorPosY;
+    ConLibGetCursorLocation(&cursorPosX, &cursorPosY);
     ConLibSetTextColor(FOREGROUND_RED, BACKGROUND_GREEN);
     ConLibWriteString(_T("你好世界\n"));
     ConLibWriteChar(_T('H'));
