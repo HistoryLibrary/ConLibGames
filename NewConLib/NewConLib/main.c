59dc158312f01ebc488f576c58f6351a4914590d
zmcj21 <96552328+zmcj21@users.noreply.github.com>
2022-07-13 12:07:31 +0300

    impl ConLibReadString

--- NewConLib/NewConLib/main.c
+++ NewConLib/NewConLib/main.c
@@ -1,6 +1,8 @@
 ﻿#define NEW_CONLIB_IMPLEMENTATION
 #include "CONLIB.H"
 
+#define READ_BUF_SIZE 256
+
 int main()
 {
     ConLibSetTitle(_T("NewConLib Example"));
@@ -9,6 +11,8 @@ int main()
     ConLibSetTextColor(FOREGROUND_RED, BACKGROUND_GREEN);
     ConLibWriteString(_T("你好世界\n"));
     ConLibWriteChar(_T('H'));
+    TCHAR readBuf[READ_BUF_SIZE];
+    int read = ConLibReadString(readBuf, READ_BUF_SIZE);
 #ifdef _DEBUG
     system("pause");
 #endif
